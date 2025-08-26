f25789b3ce537662afe3305d2f2fb442458a9989
Yuvraj Singh <uvi58493@gmail.com>
2025-08-26 18:14:13 +0530

    feat: establish database connection in server setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express, { response } from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 // app config
 const app = express();
@@ -9,10 +10,13 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
+// db connection
+connectDB();
+
 app.get("/", (request, response) => {
   response.send("API Working");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
